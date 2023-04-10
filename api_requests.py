2bd3fb0d2bbed1a42afde7b144d2f535cd7d3f51
Virgil <benjamin.dromard@gmail.com>
2023-04-10 19:16:07 +0200

    sauvegarde avec pymongo

--- api_requests.py
+++ api_requests.py
@@ -1,5 +1,23 @@
 import requests as req
 import json
+from pymongo import *
+import datetime
+
+
+# Database access
+client = MongoClient()
+db = client.testDB
+test_collection = db.testCollection
+posts = db.posts
+
+# first_book = {"author": "Joseph Conrad",
+#               "title": "Heart of Darkness",
+#               "tags": ["literature", "english"],
+#               "date": datetime.datetime.utcnow()}
+#
+# post_book = posts.insert_one(first_book)
+
+# API request and insert to database
 
 try:
     response = req.get("https://openlibrary.org/isbn/9780140328721.json")
@@ -9,8 +27,11 @@ try:
     pretty_json = json.dumps(response_as_json, sort_keys=True, indent=4)
 except Exception as err:
     print(f'An Exception has occurred: {err}')
+except TypeError as type_err:
+    print(f'Incorrect type file: {type_err}')
 else:
-    with open("response.txt", "w") as data:
-        data.write(f"{response_as_json['title']}")
-        data.write(pretty_json)
-        data.write(f"{response_as_json['authors']}")
+    with open("data.json", "w") as data:
+        data_to_write = data.write(pretty_json)
+    with open("data.json") as file:
+        file_data = json.load(file)
+    posts.insert_one(file_data)
