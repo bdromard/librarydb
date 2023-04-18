79d88f36b725db58579017addcc7a1656f5886a7
Virgil <benjamin.dromard@gmail.com>
2023-04-18 11:09:00 +0200

    Sauvegarde avec fonction add_by_isbn et add_by_title

--- api_requests.py
+++ api_requests.py
@@ -4,8 +4,7 @@ from pymongo import *
 import xmltodict
 import tkinter
 
-isbn = "%229782707321213%22&"
-
+API_URL = "http://catalogue.bnf.fr/api/SRU?version=1.2&operation=searchRetrieve&query="
 # ---------------------------------DATABASE CODE------------------------------------------------------------------------
 # Database access
 client = MongoClient()
@@ -13,38 +12,70 @@ db = client.testDB
 test_collection = db.testCollection
 posts = db.posts
 
-# first_book = {"author": "Joseph Conrad",
-#               "title": "Heart of Darkness",
-#               "tags": ["literature", "english"],
-#               "date": datetime.datetime.utcnow()}
-#
-# post_book = posts.insert_one(first_book)
+# Fonction pour vérifier le format de l'ISBN, si incorrect ou comportant un tiret ;
+# l'API de la BNF n'accepte pas de tiret.
+def check_isbn(isbn):
+    if not isbn.startswith("978"):
+        print("Veuillez insérer un ISBN correct.")
+        exit()
+    elif isbn.startswith("978-"):
+        isbn_split = isbn.split('-')
+        isbn_join = ''.join(isbn_split)
+        return isbn_join
 
-# API request and insert to database
+# Fonction de requête API et d'ajout dans la BDD par ISBN.
+def add_by_isbn(isbn):
+    # API request and insert to database
+    isbn = check_isbn(isbn)
+    try:
+        # Requête vers le catalogue général de la BNF
+        response = req.get(
+            f'{API_URL}bib.isbn%20any%20"{isbn}"&recordSchema=dublincore')
+        response.raise_for_status()
+        response_xml = xmltodict.parse(response.content)
+        # Méthode de la librairie json pour mettre en forme le fichier JSON ; type => string.
+        if response_xml["srw:searchRetrieveResponse"]["srw:numberOfRecords"] == "0":
+            print("La recherche n'a pas renvoyé de résultat, essayez d'autres critères.")
+            exit()
+    except Exception as err:
+        print(f'An Exception has occurred: {err}')
+        print("L'ISBN ne renvoie pas de données exploitables. Essayez un autre type de recherche.")
+    else:
+        pretty_response = json.dumps(
+            response_xml["srw:searchRetrieveResponse"]["srw:records"]["srw:record"]["srw:recordData"],
+            sort_keys=True,
+            indent=4)
+        # Inscription de la réponse dans un fichier JSON, puis lecture pour pouvoir être inséré dans la BDD MongoDB.
+        with open("data.json", "w") as data:
+            data.write(pretty_response)
+        with open("data.json") as file:
+            file_data = json.load(file)
+        posts.insert_one(file_data)
 
-try:
-    # Requête vers OpenLibrary
-    # response = req.get("https://openlibrary.org/isbn/9780140328721.json")
-
-    # Requête vers le catalogue général de la BNF
-    response = req.get(
-        f"http://catalogue.bnf.fr/api/SRU?version=1.2&operation=searchRetrieve&query=bib.isbn%20any%20{isbn}&recordSchema=dublincore")
-    response.raise_for_status()
-    response_xml = xmltodict.parse(response.content)
-
-    # Méthode de la librairie json pour mettre en forme le fichier JSON ; type => string.
-    pretty_response = json.dumps(
-        response_xml["srw:searchRetrieveResponse"]["srw:records"]["srw:record"]["srw:recordData"],
-        sort_keys=True,
-        indent=4)
-except Exception as err:
-    print(f'An Exception has occurred: {err}')
-else:
-    # Inscription de la réponse dans un fichier JSON, puis lecture pour pouvoir être inséré dans la BDD MongoDB.
-    with open("data.json", "w") as data:
-        data_to_write = data.write(pretty_response)
-    with open("data.json") as file:
-        file_data = json.load(file)
-    test_collection.insert_one(file_data)
+def add_by_title(title):
+    try:
+        response = req.get(
+            f'{API_URL}bib.title%20any%20"{title}"&recordSchema=dublincore&maximumRecords=1')
+        response.raise_for_status()
+        response_xml = xmltodict.parse(response.content)
+        pretty_response = json.dumps(
+            response_xml["srw:searchRetrieveResponse"]["srw:records"]["srw:record"]["srw:recordData"]["oai_dc:dc"],
+            sort_keys=True,
+            indent=4)
+    except Exception as err:
+        print(f'An Exception has occurred: {err}')
+    else:
+        with open("data.json", "w") as data:
+            data.write(pretty_response)
+        with open("data.json") as file:
+            file_data = json.load(file)
+        posts.insert_one(file_data)
 
 # ---------------------------------------------------INTERFACE CODE-----------------------------------------------------
+
+# isbn_input = str(input("Quel est l'ISBN que vous voulez ajouter ?"))
+#
+# add_by_isbn(isbn_input)
+
+# title_input = input("Quel est le titre que vous voulez ajouter ?")
+# add_by_title(title_input)
