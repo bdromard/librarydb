1cca5645150711763e93d768d0788ba6dab52bb1
Virgil <benjamin.dromard@gmail.com>
2023-05-11 15:29:45 +0200

    Sauvegarde avec fonction create par type de widget

--- gui.py
+++ gui.py
@@ -11,26 +11,34 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("LibraryDB")
         self.group_box = QtWidgets.QGroupBox(title="Box pour l'ajout de références bibliographiques")
         self.setCentralWidget(self.group_box)
-        self.create_widgets()
-
-    def create_widgets(self):
+        self.create_labels()
+        self.create_inputs()
+        self.create_buttons()
+        self.set_buddies()
+    def create_labels(self):
         # Création des différents widgets utilisables dans la GroupBox
         self.isbn_label = QtWidgets.QLabel("ISBN du livre", self)
         self.isbn_label.setGeometry(10, 30, 100, 30)
         self.title_label = QtWidgets.QLabel("Titre du livre", self)
         self.title_label.setGeometry(10, 70, 100, 30)
-        self.add_isbn_btn = QtWidgets.QPushButton("Ajouter ISBN", self)
-        self.add_isbn_btn.setGeometry(310, 30, 100, 30)
-        self.add_isbn_btn.clicked.connect(self.isbn_button_clicked)
-        self.add_title_btn = QtWidgets.QPushButton("Ajouter Titre", self)
-        self.add_title_btn.setGeometry(310, 70, 100, 30)
-        self.add_title_btn.clicked.connect(self.title_button_clicked)
+
+    def create_inputs(self):
         self.isbn_input = QtWidgets.QLineEdit('Ajout par ISBN du livre', self)
         self.isbn_input.setGeometry(105, 30, 200, 30)
         self.isbn_input.mousePressEvent = self._mousePressEvent_isbn
         self.title_input = QtWidgets.QLineEdit('Ajout par titre du livre', self)
         self.title_input.setGeometry(105, 70, 200, 30)
         self.title_input.mousePressEvent = self._mousePressEvent_title
+
+    def create_buttons(self):
+        self.add_isbn_btn = QtWidgets.QPushButton("Ajouter ISBN", self)
+        self.add_isbn_btn.setGeometry(310, 30, 100, 30)
+        self.add_isbn_btn.clicked.connect(self.isbn_button_clicked)
+        self.add_title_btn = QtWidgets.QPushButton("Ajouter Titre", self)
+        self.add_title_btn.setGeometry(310, 70, 100, 30)
+        self.add_title_btn.clicked.connect(self.title_button_clicked)
+
+    def set_buddies(self):
         # setBuddy permet de focaliser l'input du clavier sur le label choisi.
         self.isbn_label.setBuddy(self.isbn_input)
         self.title_label.setBuddy(self.title_input)
