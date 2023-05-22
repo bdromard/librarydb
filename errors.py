00adb8059a396fbfbe5220a20071c8a227ba5390
Virgil <benjamin.dromard@gmail.com>
2023-05-22 16:19:14 +0200

    Errors commented

--- errors.py
+++ errors.py
@@ -1,4 +1,4 @@
-# Exception créée lorsque la recherche vers l'API de la BNF ne renvoie pas de résultats.
+# Exception raised when BNF API request show no results.
 class ResultError(Exception):
 
     def __init__(self, search, message="La recherche n'a pas renvoyé de résultat, essayez d'autres critères."):
@@ -7,20 +7,21 @@ class ResultError(Exception):
         super().__init__(self.message)
 
 
-# Exception créée lorsque le format de l'ISBN n'est pas correct.
+# Exception raised when ISBN format is not correct.
 class IncorrectISBNFormat(Exception):
 
     def __init__(self, message="Veuillez insérer un ISBN au bon format."):
         self.message = message
         super().__init__(self.message)
 
-# Exception créée lorsque la référence existe déjà dans la collection de la base de données.
+# Exception raised when reference already exists in collection.
 class ExistingReference(Exception):
 
     def __init__(self, message="Cette référence est déjà présente dans votre collection."):
         self.message = message
         super().__init__(self.message)
 
+# Exception raised when collection already exists in database.
 class ExistingCollection(Exception):
 
     def __init__(self, message="Cette collection existe déjà"):
